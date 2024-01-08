87a030c35fa13a9c20f8aa38af0e7e1c79bc2b80
Pierre Rubens Milorme <prubensmilorme@gmail.com>
2024-01-08 12:32:35 -0800

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MapComponent } from './map/map.component';
+import { StatsComponent } from './stats/stats.component';
+import { ControlsComponent } from './controls/controls.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ControlsComponent, MapComponent, StatsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'tpe-reloaded';
+  title = 'TPE reloaded with Angular';
 }
---
+++ src/app/map/map.component.ts
@@ -0,0 +1,16 @@
+import { AfterViewInit, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-map',
+  standalone: true,
+  imports: [],
+  templateUrl: './map.component.html',
+  styleUrl: './map.component.scss'
+})
+export class MapComponent implements AfterViewInit{
+  constructor() {}
+
+  ngOnInit() {}
+
+  ngAfterViewInit(): void {}
+}
